b5e15ea241b4b1dd8251d3f99672410bae5b1650
AyeIP <50221550+AyeIP@users.noreply.github.com>
2020-04-03 23:05:09 -0300

    Primera actividad

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+
+int main()
+{
+    char texto [200];
+	 
+	std::cout << "ingrese su texto: \n";
+	std::cin >> texto;
+
+	std::cout << "su texto es: " << texto << std::endl;
+
+	return 0;
+}
